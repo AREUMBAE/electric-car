e4eee9feb456eccb7dcba8441efb77cfcb3c0960
AREUMBAE <aureum0118@gmail.com>
2023-10-06 19:50:37 +0900

    1
    1

--- src/main/java/demo_final/DemoFinalApplication.java
+++ src/main/java/demo_final/DemoFinalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoFinalApplication {
 
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoFinalApplication.class, args);
 	}
